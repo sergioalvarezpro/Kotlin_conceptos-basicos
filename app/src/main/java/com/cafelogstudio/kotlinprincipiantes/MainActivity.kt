c1eb37e6f6f50ce17f03c3582841610c5130c1c5
sergioalvarezpro <ealvser@gmail.com>
2022-10-14 18:29:15 +0200

    unit-1_variables y constantes
    Finalizado variable y constantes. Inicio unit-2, tipos de datos

--- app/src/main/java/com/cafelogstudio/kotlinprincipiantes/MainActivity.kt
+++ app/src/main/java/com/cafelogstudio/kotlinprincipiantes/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         variablesYConstantes()
+
+        tiposDeDatos()
     }
 
     /* Aqui vamos a hablar de variables y constantes */
@@ -16,8 +18,36 @@ class MainActivity : AppCompatActivity() {
     private fun variablesYConstantes(){
 
         // Variables
+        // Las variables pueden cambiar el dato, pero no el tipo de dato (Int, string, boolean)
         var myFirstVariable = "Hello Hackermen!"
-
         println(myFirstVariable)
+
+        myFirstVariable = "Bienvenidos a MoureDev"
+        println(myFirstVariable)
+
+        //No podemos asignar un tipo de dato Int a una variable de myFirstVariable = 1
+        var mySecondVariable = "Suscribete"
+        println(mySecondVariable)
+
+        mySecondVariable = myFirstVariable
+        println(mySecondVariable)
+
+        myFirstVariable = "¿Ya te has suscrito?"
+        println(myFirstVariable)
+
+        //Constantes
+        val myFirstConstant = "¿Te ha gustado el tutorial?"
+        println(myFirstConstant)
+
+        //Una constante no puede modificar su valor:
+        //myFirstConstant = "Si te ha gustado, dale a LIKE"
+        val mySecondConstant = myFirstVariable
+        println(mySecondConstant)
+    }
+
+    /* Aquí vamos hablar de tipos de datos (data types) */
+
+    private fun tiposDeDatos(){
+
     }
 }
